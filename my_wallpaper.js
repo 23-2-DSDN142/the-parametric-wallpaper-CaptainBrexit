bc84d6155dad2f27522bf217666208b8ba2c3686
CaptainBrexit <toby.balls@me.com>
2023-07-31 10:29:59 +1200

    wallpaper

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,4 @@
-//your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+var gapBetweenLines = 10;
 
 
 function setup_wallpaper(pWallpaper) {
@@ -18,6 +16,69 @@ function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+function my_symbol() { 
+  /*
+  for(i =  0; i < 160; i += gapBetweenLines) {  
+    fill(0);
+    quad(i, i + 40, i + 5, i + 45, i + 45, i + 5, i + 40, i);
+  }
+ 
+  noFill();
+  strokeWeight(8);
+  quad(40, 5, 5, 40, 155, 195, 195, 155);
+
+  */
+  noStroke();
+  beginShape(TESS);
+  vertex(40, 0);
+  vertex(0, 0);
+  vertex(0, 40);
+  vertex(60, 100);
+  vertex(0, 160);
+  vertex(0,200);
+  vertex(40, 200);
+  vertex(100, 140);
+  vertex(160, 200);
+  vertex(200, 200);
+  vertex(200, 160);
+  vertex(140, 100);
+  vertex(200, 40);
+  vertex(200, 0);
+  vertex(160, 0);
+  vertex(100, 60);
+  endShape(CLOSE);
+
+  beginShape(TRIANGLES);
+  vertex(0, 60);
+  vertex(0, 140);
+  vertex(40, 100);
+
+  vertex(60, 0);
+  vertex(140, 0);
+  vertex(100, 40);
+
+  vertex(200, 60);
+  vertex(200, 140);
+  vertex(160, 100);
+  
+  vertex(60, 200);
+  vertex(140, 200);
+  vertex(100, 160);
+  endShape();
+/*
+noFill();
+bezier(0, 1, 50, 200, 150, 50, 200, 200);
+
+bezier(90, 117, 110, 120, 130, 107, 135, 102);
+line(135, 102, 135, 125);
+line(135, 102, 140, 102);
+line(140, 102, 140, 126);
+
+circle(174, 60, 4, 4);
+line(174, 62, 174, 70);
+line(174, 70, 172, 72);
+line(174, 70, 176, 72);
+line(170, 65, 178, 65);
+
+*/
 }
